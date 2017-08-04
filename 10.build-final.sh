7a3a540c0a37a97882e8cfbc0d171527553cd8b5
Petr Kulhanek <kulhanek@chemi.muni.cz>
2017-08-04 14:15:24 +0200

    polishing

--- 10.build-final.sh
+++ 10.build-final.sh
@@ -54,13 +54,18 @@ cat > $SOFTBLDS/$NAME:$VERS:$ARCH:$MODE.bld << EOF
 EOF
 if [ $? -ne 0 ]; then exit 1; fi
 
-ams-map-manip addbuilds $SITES $NAME:$VERS:$ARCH:$MODE
-if [ $? -ne 0 ]; then exit 1; fi
+echo ""
+echo "Adding builds ..."
+ams-map-manip addbuilds $SITES $NAME:$VERS:$ARCH:$MODE >> ams.log 2>&1
+if [ $? -ne 0 ]; then echo ">>> ERROR: see ams.log"; exit 1; fi
 
+echo "Distribute builds ..."
 ams-map-manip distribute >> ams.log 2>&1
-if [ $? -ne 0 ]; then exit 1; fi
+if [ $? -ne 0 ]; then echo ">>> ERROR: see ams.log"; exit 1; fi
 
+echo "Rebuilding cache ..."
 ams-cache rebuildall >> ams.log 2>&1
+if [ $? -ne 0 ]; then echo ">>> ERROR: see ams.log"; exit 1; fi
 
 echo "Log file: ams.log"
 echo ""
